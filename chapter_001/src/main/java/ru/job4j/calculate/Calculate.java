8a3473750402046d3e18c9382ad84e40cae3cd13
DonJke <iljadanilov@mail.ru>
2018-12-22 18:09:16 +0700

    Uncle Sasha is a dirty dog

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -5,10 +5,20 @@
 @version 1
 */
 public class Calculate {
-/**
-@return Hello World
-*/	
-		public static void main(String[] args) {
-			System.out.println("Hello World");
-		}
+    /**
+     * Main.
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    /**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
 }
---
+++ chapter_001/src/test/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,7 @@
+/**
+* Package for calculate test.
+*
+* @author Ilya Danilov
+* @version 1
+* @since 22.12.2018
+*/
