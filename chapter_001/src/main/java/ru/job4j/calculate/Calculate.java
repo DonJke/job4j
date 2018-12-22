e0022e5b42cdc475988d660190891d46aa648620
DonJke <iljadanilov@mail.ru>
2018-12-22 16:52:21 +0700

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,13 @@
  package ru.job4j.calculate;
-
+/**
+@author Ilya Danilov
+@since 22.12.2018
+@version 1
+*/
 public class Calculate {
-	
+/**
+@return Hello World
+*/	
 		public static void main(String[] args) {
 			System.out.println("Hello World");
 		}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,7 @@
+/**
+* Package for calculate task.
+*
+* @author Ilya Danilov
+* @version 1
+* @since 22.12.2018
+*/
