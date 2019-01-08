93caaaf76fee474eb77af07f59e8fb9f9d828ac9
DonJke <iljadanilov@mail.ru>
2019-01-08 14:29:34 +0700

    Конвертер валюты

--- chapter_001/src/main/java/ru/job4j/calculate/Converter.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Converter.java
@@ -11,7 +11,8 @@ public class Converter {
      * @return Евро.
      */
     public int rubleToEuro(int value) {
-        return -1;
+		return value / 80;
+
     }
 
     /**
@@ -20,6 +21,23 @@ public class Converter {
      * @return Доллары
      */
     public int rubleToDollar(int value) {
-        return -1;
+        return value / 70;
+    }
+	/**
+     * Конвертируем евро в рубли.
+     * @param value евро.
+     * @return рубли.
+     */
+    public int euroToRuble(int value) {
+		return value * 80;
+
+    }
+	/**
+     * Конвертируем доллары в рубли.
+     * @param value доллары.
+     * @return рубли
+     */
+    public int dollarToRuble(int value) {
+        return value * 70;
     }
 }
