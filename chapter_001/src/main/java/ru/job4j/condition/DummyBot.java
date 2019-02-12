210cbd6b96a632705882a79b862683003b9afc05
DonJke <iljadanilov@mail.ru>
2019-02-12 20:50:18 +0700

    Dummy Bot

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,24 @@
+package ru.job4j.condition;
+/**
+ * Бот отвечающий на три вопроса
+ * @author Ilya Danilov
+ * @version 1
+ * @since 12.02.2019
+ */
+
+public class DummyBot {
+    /**
+     * Отвечает на вопросы.
+     * @param question Вопрос от клиента.
+     * @return Ответ.
+     */
+    public String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            rsl = "Привет, умник.";
+        } else if ("Пока.".equals(question)) {
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+}
