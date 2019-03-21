43007c02d3af1cecb07e3b8e6b837b1b0da71d98
DonJke <iljadanilov@mail.ru>
2019-03-21 19:05:37 +0700

    Tiangle

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,12 @@
+package ru.job4j.max;
+/**
+ * Вычесление максимального числа из двух чисел
+ * @author Ilya Danilov
+ * @version 1
+ * @since 13.02.2019
+ */
+public class Max {
+    public int max(int first, int second) {
+        return first > second ? first : second;
+    }
+}
