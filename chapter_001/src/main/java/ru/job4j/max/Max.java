cd9e853217b0d709c8dfc8740ef44e4218633e7d
DonJke <iljadanilov@mail.ru>
2019-03-21 20:29:44 +0700

    Treangle

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -1,12 +1,14 @@
 package ru.job4j.max;
 /**
- * Вычесление максимального числа из двух чисел
+ * Вычесление максимального числа из трех чисел
  * @author Ilya Danilov
  * @version 1
- * @since 13.02.2019
+ * @since 21.03.2019
  */
 public class Max {
     public int max(int first, int second) {
-        return first > second ? first : second;
+        public max(int first, int second, int third)
+        int temp = this.max(first, second, third);
+        return temp;
     }
 }
